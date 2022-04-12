97f18093b32871145fed20297f617a01c77b0d3b
Lluís Camino <caminoperez.lluis@gmail.com>
2022-04-12 13:53:38 +0200

    Set System look and feel

--- src/main/java/ui/ApplicationUI.java
+++ src/main/java/ui/ApplicationUI.java
@@ -4,11 +4,13 @@ import logic.controller.Controller;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.UIManager;
 import java.awt.HeadlessException;
 
 public class ApplicationUI extends JFrame {
     public ApplicationUI(Controller controller) throws HeadlessException {
         super("Compresor de archivos Huffman");
+        setSystemLookAndFeel();
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         JPanel mainPanel = new JPanel();
@@ -16,5 +18,13 @@ public class ApplicationUI extends JFrame {
         mainPanel.add(new ActionsMenu(controller, new DialogSender()));
         add(mainPanel);
         pack();
+    }
+
+    private void setSystemLookAndFeel() {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
