8ca89ead5caaa31f255e512ba6684aa0d8adf38c
Lluís Camino <caminoperez.lluis@gmail.com>
2022-04-12 13:56:55 +0200

    Add success messages to ActionsMenu

--- src/main/java/ui/ActionsMenu.java
+++ src/main/java/ui/ActionsMenu.java
@@ -1,6 +1,7 @@
 package ui;
 
 import logic.controller.Controller;
+import logic.model.transformation.Transformation;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
@@ -11,6 +12,8 @@ import java.awt.event.ActionEvent;
 import java.io.IOException;
 
 public class ActionsMenu extends JPanel {
+    private static final String[] SUCCESS_DIALOG_OPTIONS = {"Ver codificación", "Cerrar"};
+
     private final Controller controller;
     private final DialogSender dialogSender;
 
@@ -31,7 +34,13 @@ public class ActionsMenu extends JPanel {
 
     private void onCompressFile(ActionEvent event) {
         try {
-            controller.compressFile();
+            Transformation transformation = controller.compressFile();
+            dialogSender.displayOptionDialog(
+                    buildCompressionSuccessTitle(transformation),
+                    buildCompressionSuccessMessage(transformation),
+                    DialogSender.NotificationType.INFO,
+                    SUCCESS_DIALOG_OPTIONS
+            );
         } catch (IOException e) {
             dialogSender.displayAlertDialog("Error", e.getLocalizedMessage(), DialogSender.NotificationType.ERROR);
         }
@@ -39,9 +48,47 @@ public class ActionsMenu extends JPanel {
 
     private void onDecompressFile(ActionEvent event) {
         try {
-            controller.decompressFile();
+            Transformation transformation = controller.decompressFile();
+            dialogSender.displayOptionDialog(
+                    buildDecompressionSuccessTitle(transformation),
+                    buildDecompressionSuccessMessage(transformation),
+                    DialogSender.NotificationType.INFO,
+                    SUCCESS_DIALOG_OPTIONS
+            );
         } catch (IOException e) {
             dialogSender.displayAlertDialog("Error", e.getLocalizedMessage(), DialogSender.NotificationType.ERROR);
         }
+    }
+
+    private String buildCompressionSuccessTitle(Transformation transformation) {
+        String inputFileName = transformation.inputFile().getName();
+        return "¡Fichero " + inputFileName + " comprimido con éxito!";
+    }
+
+    private String buildCompressionSuccessMessage(Transformation transformation) {
+        return String.format("""
+                        - Archivo comprimido: %s
+                        - Tamaño archivo original: %dB
+                        - Tamaño archivo comprimido: %dB
+                        - Ratio compresión: %f
+                        - Tiempo compresión: %s
+                        """, transformation.outputFile().getPath(), transformation.inputFileSizeBytes(),
+                transformation.outputFileSizeBytes(), transformation.fileSizeRatio(),
+                transformation.humanReadableDuration());
+    }
+
+    private String buildDecompressionSuccessTitle(Transformation transformation) {
+        String inputFileName = transformation.inputFile().getName();
+        return "¡Fichero " + inputFileName + " extraído con éxito!";
+    }
+
+    private String buildDecompressionSuccessMessage(Transformation transformation) {
+        return String.format("""
+                        - Archivo extraído: %s
+                        - Tamaño archivo comprimido: %dB
+                        - Tamaño archivo extraído: %dB
+                        - Tiempo extracción: %s
+                        """, transformation.outputFile().getPath(), transformation.inputFileSizeBytes(),
+                transformation.outputFileSizeBytes(), transformation.humanReadableDuration());
     }
 }
